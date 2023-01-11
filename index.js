2eaf204f0099e611b385782dd367fa0bb9aa668e
Louis Moses <louismoses.rait@gmail.com>
2023-01-11 16:39:33 +0800

    fixed unordered list display

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-let myLeads = ["awesomelead.com", "epicland.com", "greatland.com"];
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  for (let i = 0; i < myLeads.length; i++) {
-    console.log(myLeads[i]);
-    ulEl.textContent += myLeads[i] + ", ";
-  }
+  renderLeads();
 });
 
-// 5:06:47
+function renderLeads() {
+  let listItems = "";
+
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>";
+  }
+  ulEl.innerHTML = listItems;
+}
+
+// 5:26:40
 
 // scratch section ---------------
+const container = document.getElementById("container");
+container.innerHTML = "<button onclick='Buy()' > Buy! </buttton>";
+
+function Buy() {
+  container.innerHTML += "<p> Thank you for buying! </p>";
+}
 
 // end scratch section ---------------
