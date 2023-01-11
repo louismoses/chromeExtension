ebb35a0c01ee5c45538965a868c8781fd22a0713
Louis Moses <louismoses.rait@gmail.com>
2023-01-11 15:45:26 +0800

    display input value in an unordered list

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-let inputBtn = document.getElementById("input-btn");
+let myLeads = ["awesomelead.com", "epicland.com", "greatland.com"];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
-  console.log("button clicked");
+  myLeads.push(inputEl.value);
+  for (let i = 0; i < myLeads.length; i++) {
+    console.log(myLeads[i]);
+    ulEl.textContent += myLeads[i] + ", ";
+  }
 });
 
-// 4:47:53
+// 5:06:47
+
+// scratch section ---------------
+
+// end scratch section ---------------
