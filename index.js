60ce7217e1e72e8dd3d59746d5159307ccd812ec
Louis Moses <louismoses.rait@gmail.com>
2023-01-13 10:10:36 +0800

    added a DELETE ALL button element, styled it and added delete function

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("mySponsors"));
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("mySponsors"));
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
@@ -16,6 +17,13 @@ inputBtn.addEventListener("click", function () {
   localStorage.setItem("mySponsors", JSON.stringify(myLeads));
   renderLeads();
   console.log(localStorage.getItem("myLeads"));
+});
+
+deleteBtn.addEventListener("dblclick", function () {
+  console.log("double clicked");
+  localStorage.clear();
+  myLeads = [];
+  renderLeads();
 });
 
 function renderLeads() {
