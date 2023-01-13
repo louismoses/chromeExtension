55efb45507c4621df671e4149160778f872d700b
Louis Moses <louismoses.rait@gmail.com>
2023-01-13 10:51:35 +0800

    SAVE TAB button added

--- index.js
+++ index.js
@@ -1,39 +1,29 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const tabBtn = document.getElementById("tab-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
-
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("mySponsors"));
+const tabs = [
+  {
+    url: "https://www.linkedin.com/in/lm-rait/",
+  },
+];
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
 }
 
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  localStorage.setItem("mySponsors", JSON.stringify(myLeads));
-  renderLeads();
-  console.log(localStorage.getItem("myLeads"));
-});
-
-deleteBtn.addEventListener("dblclick", function () {
-  console.log("double clicked");
-  localStorage.clear();
-  myLeads = [];
-  renderLeads();
-});
-
-function renderLeads() {
+function render(leads) {
   let listItems = "";
 
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
       <li>
-        <a target="_blank" href="${myLeads[i]}">
-        ${myLeads[i]}
+        <a target="_blank" href="${leads[i]}">
+        ${leads[i]}
         </a>
       </li>
       `;
@@ -41,9 +31,25 @@ function renderLeads() {
   ulEl.innerHTML = listItems;
 }
 
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("mySponsors", JSON.stringify(myLeads));
+  render(myLeads);
+});
+
+tabBtn.addEventListener("click", function () {
+  console.log(tabs[0]);
+});
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
 // 6:03:00
 
 // scratch section ---------------
-let trueOfFalse = Boolean("");
-console.log(trueOfFalse);
+
 // end scratch section ---------------
